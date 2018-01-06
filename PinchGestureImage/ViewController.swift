459d52ec971dd4a24b78ce844e11a3b8b37524e1
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-06 13:12:45 +0900

    image

--- PinchGestureImage/ViewController.swift
+++ PinchGestureImage/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgPinch: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let Pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.gestureHandler(gesture:)))
+        self.view.addGestureRecognizer(Pinch)
     }
-
+    @objc func gestureHandler(gesture: UIPinchGestureRecognizer) {
+        imgPinch.transform = imgPinch.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+        //다음 변환을 위해 속성 설정
+        gesture.scale = 1
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
